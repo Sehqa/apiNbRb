e3bbb77ee8d2ac08afb159daefa94cd9783c5e56
Sehqa <90191836+Sehqa@users.noreply.github.com>
2021-11-08 23:01:35 +0300

    Update nb_api.py

--- nb_api.py
+++ nb_api.py
@@ -6,59 +6,73 @@ from datetime import date
 from datetime import timedelta
 from currency_classes.currency_statistics import *
 from jsonschema import validate
-
+import json
 
 class NbApi:
 
     @staticmethod
+    def get_json_schema(file):
+        with open(str(file), 'r') as f:
+            data = json.loads(f.read())
+        return data
+
+
+    @staticmethod
     def get_all_statistics():
-        start_date = date.today() - timedelta(days=30)
+        start_date = date.today() - timedelta(days=count_days_current_month)
         end_date = date.today()
         list_statistics = []
         currencies = NbApi.get_currencies()[1]
         for i in range(0, len(currencies)):
             request = requests.get(url=CURRENT_DYMANICS + str(currencies[i].get('Cur_ID')) + "?startDate=" + str(
                 start_date) + "&endDate=" + str(end_date))
+            with open('statistics_schema.json') as f:
+                templates = json.load(f)
             if len(request.json()) != 0:
                 for i in range(0, len(request.json())):
-                    validate(instance=request.json()[i], schema=const_for_api.schema_statistics)
+                    validate(instance=request.json()[i], schema=templates)
                     currency = CurrencyStatistics(request.json()[i])
                     list_statistics.append(currency)
         return list_statistics
 
     @staticmethod
-    def get_statistics_for_currency(cur_id, start_date=date.today() - timedelta(days=30), end_date=date.today()):
+    def get_statistics_for_currency(cur_id, start_date=date.today() - timedelta(days=count_days_current_month), end_date=date.today()):
         const_for_api.STATUS = 0
         list_statistics = []
         request = requests.get(url=CURRENT_DYMANICS + str(
             cur_id) + "?startDate=" + str(start_date) + "&endDate=" + str(end_date))
-        if request.status_code != 200:
-            return request.status_code
-        elif len(request.json()) != 0:
+        with open('statistics_schema.json') as f:
+            templates = json.load(f)
+        if len(request.json()) != 0:
             for i in range(0, len(request.json())):
-                validate(instance=request.json()[i], schema=const_for_api.schema_statistics)
+                validate(instance=request.json()[i], schema=templates)
                 currencys = CurrencyStatistics(request.json()[i])
                 list_statistics.append(currencys)
-        return list_statistics
+        return list_statistics,request.status_code
 
     @staticmethod
     def get_currencies(list_currencies=[]):
         request = requests.get(url=ALL_CURRENCIES)
+        with open('currency_schema.json') as f:
+            templates = json.load(f)
         for i in range(0, len(request.json())):
-            validate(instance=request.json()[i], schema=const_for_api.schema_currency)
+            validate(instance=request.json()[i], schema=templates)
+
             currency_object = Currency(request.json()[i])
             list_currencies.append(currency_object)
-        return [request.status_code, request.json()]
+        return request.status_code, request.json()
 
     @staticmethod
     def get_current_currencies(cur_id):
         request = requests.get(url=ALL_CURRENCIES + '/' + cur_id)
-        validate(instance=request.json(), schema=const_for_api.schema_currency)
-        return [request.status_code, request, request.json()]
+        with open('currency_schema.json') as f:
+            templates = json.load(f)
+        validate(instance=request.json(), schema=templates)
+
+        return request.status_code, request, request.json()
 
     @staticmethod
     def custom_get_statistics_for_currency(cur_id,custom_parameter=''):
         request = requests.get(url=CURRENT_DYMANICS + str(
             cur_id) + custom_parameter)
         return request.status_code
-
