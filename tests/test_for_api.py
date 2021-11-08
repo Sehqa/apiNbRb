d6e96d78eb5a64c20d1d026c376556da3fd3057b
Sehqa <90191836+Sehqa@users.noreply.github.com>
2021-11-09 00:43:44 +0300

    Update test_for_api.py

--- tests/test_for_api.py
+++ tests/test_for_api.py
@@ -11,92 +11,44 @@ def test_get_random_currency():
     assert (NbApi.get_current_currencies(DbTools.get_random_currency())[0] == 200)
 
 
-@pytest.mark.parametrize('cur_id', [1111111])
-def test_get_random_currency_invalid(cur_id):
-    assert (NbApi.get_current_currencies(str(cur_id))[0] == 404)
+@pytest.mark.parametrize('cur_id', [1111111,'fddfdfdfd'])
+@pytest.mark.parametrize('status_code', [404,400])
+def test_get_random_currency(cur_id,status_code):
+    assert (NbApi.get_current_currencies(str(cur_id))[0] == status_code)
 
 
-@pytest.mark.parametrize('cur_id', ['fddfdfdfd'])
-def test_get_random_currency_text(cur_id):
-    assert (NbApi.get_current_currencies(str(cur_id))[0] == 400)
 
 
 def test_compare_all_currencies(fixture_all_id):
-    CompareTools.compare_random_currency(fixture_all_id)
+    CompareTools.compare_json_and_db_currency(fixture_all_id)
 
 
 def test_compare_currency_statistics(fixture_random_statistics_from_db, fixture_random_statistics_from_api):
-    CompareTools.compare_random_statistics(fixture_random_statistics_from_db, fixture_random_statistics_from_api)
+    fixture_random_statistics_from_api == fixture_random_statistics_from_db
 
 
-@pytest.mark.parametrize('cur_id', ['111111111111'])
-def test_get_currency_statistics_with_invalid_id(cur_id):
-    assert NbApi.get_statistics_for_currency(cur_id) == 404
-
-
-
-@pytest.mark.parametrize('cur_id', ['аывыавы'])
-def test_get_currency_statistics_with_invalid_value_in_id(cur_id):
-    assert NbApi.get_statistics_for_currency(cur_id) == 400
-
-
-@pytest.mark.parametrize('cur_id', ['510'])
-@pytest.mark.parametrize('start_date', ['2021-12-03'])
-@pytest.mark.parametrize('end_date', ['2021-11-02'])
-def test_get_currency_statistics_with_invalid_date(cur_id,start_date,end_date):
-    assert NbApi.get_statistics_for_currency(cur_id,start_date,end_date ) == 400
+@pytest.mark.parametrize('cur_id', ['510','аывыавы'])
+@pytest.mark.parametrize('status_code', [200,400])
+def test_get_currency_statistics(cur_id,status_code):
+    assert NbApi.get_statistics_for_currency(cur_id)[1] == status_code
 
 
 
-@pytest.mark.parametrize('cur_id', ['510'])
-@pytest.mark.parametrize('start_date', [''])
-@pytest.mark.parametrize('end_date', ['2021-11-02'])
-def test_get_currency_statistics_with_invalid_date2(cur_id,start_date,end_date):
-    assert NbApi.get_statistics_for_currency(cur_id,start_date,end_date ) == 400
+@pytest.mark.parametrize('cur_id', ['510','510',''])
+@pytest.mark.parametrize('start_date', ['2021-11-02','','2021-12-03'])
+@pytest.mark.parametrize('end_date', ['2021-12-02','2021-11-02',''])
+@pytest.mark.parametrize('status_code', [200,400,400])
+def test_get_currency_statistics_with_invalid_date(cur_id, start_date, end_date,status_code):
+    assert NbApi.get_statistics_for_currency(cur_id, start_date, end_date)[1] == status_code
 
 
 
-@pytest.mark.parametrize('cur_id', ['510'])
-@pytest.mark.parametrize('start_date', [''])
-@pytest.mark.parametrize('end_date', [''])
-def test_get_currency_statistics_with_invalid_date3(cur_id,start_date,end_date):
-    assert NbApi.get_statistics_for_currency(cur_id,start_date,end_date ) == 400
+@pytest.mark.parametrize('cur_id', ['510','510','510'])
+@pytest.mark.parametrize('start_date', ['2021-11-02','','2021-11-02'])
+@pytest.mark.parametrize('end_date', ['2021-12-03','2021-12-03',''])
+@pytest.mark.parametrize('status_code', [200,404,404])
+def test_witout_parameters(cur_id, start_date, end_date,status_code):
+    assert NbApi.custom_get_statistics_for_currency(cur_id, custom_parameter='') == status_code
 
 
 
-@pytest.mark.parametrize('cur_id', ['510'])
-@pytest.mark.parametrize('start_date', ['2021-12-03'])
-@pytest.mark.parametrize('end_date', [''])
-def test_get_currency_statistics_with_invalid_date4(cur_id,start_date,end_date):
-    assert NbApi.get_statistics_for_currency(cur_id,start_date,end_date ) == 400
-
-
-@pytest.mark.parametrize('cur_id', ['510'])
-@pytest.mark.parametrize('start_date', ['2021-10-03'])
-@pytest.mark.parametrize('end_date', ['2321-12-03'])
-def test_get_currency_statistics_with_invalid_date5(cur_id,start_date,end_date):
-    assert NbApi.get_statistics_for_currency(cur_id,start_date,end_date ) !=400
-
-@pytest.mark.parametrize('cur_id', [''])
-@pytest.mark.parametrize('start_date', ['2021-11-02'])
-@pytest.mark.parametrize('end_date', ['2021-12-03'])
-def test_get_currency_statistics_without_cur_id(cur_id,start_date,end_date):
-    assert NbApi.get_statistics_for_currency(cur_id,start_date,end_date ) == 400
-
-
-
-@pytest.mark.parametrize('cur_id', ['510'])
-def test_witout_parameters(cur_id):
-    assert NbApi.custom_get_statistics_for_currency(cur_id, custom_parameter='' == 400
-
-
-@pytest.mark.parametrize('cur_id', ['510'])
-@pytest.mark.parametrize('end_date', ['2021-12-03'])
-def test_witout_parameter_start_date(cur_id, end_date):
-    assert NbApi.custom_get_statistics_for_currency(cur_id, custom_parameter="?endDate=" + str(end_date)) == 400
-
-
-@pytest.mark.parametrize('cur_id', ['510'])
-@pytest.mark.parametrize('start_date', ['2021-11-02'])
-def test_witout_parameter_end_date(cur_id, start_date):
-    assert NbApi.custom_get_statistics_for_currency(cur_id, custom_parameter="?startDate=" + str(start_date)) == 400
