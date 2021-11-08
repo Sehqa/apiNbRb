894a152aa0eb995f9649b4e0ed4b85c9ea600a40
Sehqa <90191836+Sehqa@users.noreply.github.com>
2021-11-08 23:00:37 +0300

    Update db_tools.py

--- tools/db_tools.py
+++ tools/db_tools.py
@@ -20,23 +20,19 @@ class DbTools:
         conn.close()
         return random.choice(result_list)
 
-    # добавляем одну запись валют
+    # добавляем одну забись валют
     @staticmethod
     def add_currency_to_db(currency):
         conn = cx_Oracle.connect(CONNECT)
         curs = conn.cursor()
         sql_req = (
-                'INSERT INTO CURRENCY (CUR_ID,CUR_PARENT_ID,CUR_CODE,CUR_ABBREVIATION,CUR_NAME,CUR_NAME_BEL,CUR_NAME_ENG,CUR_QUOT_NAME,CUR_QUOT_NAME_BEL,CUR_QUOT_NAME_ENG,CUR_NAME_MULTI,CUR_NAME_BEL_MULTI,CUR_NAME_ENG_MULTI,CUR_SCALE,CUR_PERIODICITY,CUR_DATE_START,CUR_DATE_END) VALUES (\'' + str(
-            currency.cur_id) + '\',\'' + str(currency.cur_parent_id) + '\',\'' + str(
-            currency.cur_code) + '\',\'' + str(currency.cur_abbreviation) + '\',\'' + str(
-            currency.cur_name) + '\',\'' + str(currency.cur_name_bel).replace("'", "") + '\',\'' + str(
-            currency.cur_name_eng) + '\',\'' + str(currency.cur_quot_name) + '\',\'' + str(
-            currency.cur_quot_name_bel).replace("'", "") + '\',\'' + str(
-            currency.cur_quot_name_eng) + '\',\'' + str(currency.cur_name_multi) + '\',\'' + str(
-            currency.cur_name_bel_multi).replace("'", "") + '\',\'' + str(
-            currency.cur_name_eng_multi) + '\',\'' + str(currency.cur_scale) + '\',\'' + str(
-            currency.cur_periodicity) + '\',\'' + str(currency.cur_date_start) + '\',\'' + str(
-            currency.cur_date_end) + '\')')
+                'INSERT INTO CURRENCY (CUR_ID,CUR_PARENT_ID,CUR_CODE,CUR_ABBREVIATION,CUR_NAME,CUR_NAME_BEL,CUR_NAME_ENG,CUR_QUOT_NAME,CUR_QUOT_NAME_BEL,CUR_QUOT_NAME_ENG,CUR_NAME_MULTI,CUR_NAME_BEL_MULTI,CUR_NAME_ENG_MULTI,CUR_SCALE,CUR_PERIODICITY,CUR_DATE_START,CUR_DATE_END) VALUES (\'' + '{}' + '\',\'' + '{}' + '\',\'' +
+                '{}' + '\',\'' + '{}' + '\',\'' + '{}' + '\',\'' + '{}' + '\',\'' + '{}' + '\',\'' + '{}' + '\',\'' + '{}' + '\',\'' + '{}' + '\',\'' + '{}' + '\',\'' + '{}' + '\',\'' + '{}' + '\',\'' + '{}' + '\',\'' + '{}' + '\',\'' + '{}' + '\',\'' + '{}' + '\')').format(
+            currency.cur_id, currency.cur_parent_id, currency.cur_code, currency.cur_abbreviation, currency.cur_name,
+            str(currency.cur_name_bel).replace("'", ""), currency.cur_name_eng, currency.cur_quot_name,
+            str(currency.cur_quot_name_bel).replace("'", ""), currency.cur_quot_name_eng, currency.cur_name_multi,
+            str(currency.cur_name_bel_multi).replace("'", ""), currency.cur_name_eng_multi, currency.cur_scale,
+            currency.cur_periodicity, currency.cur_date_start, currency.cur_date_end)
         curs.execute(sql_req)
         conn.commit()
         conn.close()
@@ -82,8 +78,6 @@ class DbTools:
         conn.close()
         return random.choice(result_list)
 
-
-
     # тянем из бд конкретную статистику
     @staticmethod
     def get_currency_from_statistics_db(cur_id):
@@ -96,14 +90,13 @@ class DbTools:
         conn.close()
         return result_list
 
-    # добавляем в бд запись со статистикой
     @staticmethod
     def add_statistics_to_db(currency_statistics):
         conn = cx_Oracle.connect(CONNECT)
         curs = conn.cursor()
-        sql_req = ('INSERT INTO CURRENCY_STATISTICS (CUR_ID,CUR_DATE,CUR_OFFICIAL_RATE) VALUES (\'' + str(
-            currency_statistics.cur_id) + '\',\'' + str(currency_statistics.date) + '\',\'' + str(
-            currency_statistics.cur_official_rate) + '\')')
+        sql_req = (
+                    'INSERT INTO CURRENCY_STATISTICS (CUR_ID,CUR_DATE,CUR_OFFICIAL_RATE) VALUES (\'' + '{}' + '\',\'' + '{}' + '\',\'' + '{}' + '\')').format(
+            currency_statistics.cur_id, currency_statistics.date, currency_statistics.cur_official_rate)
         curs.execute(sql_req)
         conn.commit()
         conn.close()
@@ -120,8 +113,15 @@ class DbTools:
         conn.close()
 
     @staticmethod
+    def check_is_empty(currency):
+
+        return currency
+
+    @staticmethod
     def add_all_statistics_to_db(list_statistics=NbApi.get_all_statistics()):
         for i in list_statistics:
+            if i.cur_name_multi == '':
+                i.cur_name_multi = 'emptyvalue'
             DbTools.add_statistics_to_db(i)
 
     @staticmethod
@@ -130,6 +130,7 @@ class DbTools:
         conn = cx_Oracle.connect(CONNECT)
         curs = conn.cursor()
         sql_req = ('SELECT * FROM CURRENCY_STATISTICS')
+        for row in curs.execute(sql_req):
+            print(row[0], row[1], row[3])
         conn.close()
         return result_list
-
